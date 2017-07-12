c4829973d73797f3c57cb74550630fddc0d482f4
Seolhee Kim <seolhee@upspringmedia.com>
2017-07-11 17:43:17 -1000

    deleted html test created files, added Get Method

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const http = require('http');
 const fs = require('fs');
 const query = require('querystring')
 
+// //Read contents of files
+// let index = fs.readFileSync('./public/index.html')
+// let helium = fs.readFileSync('./public/helium.html')
+// let hydrogen = fs.readFileSync('./public/hydrogen.html')
+// let error = fs.readFileSync('./public/404.html')
+// let css = fs.readFileSync('./public/css/styles.css')
+
+
+
 function postRespond(elementName, elementSymbol, elementAtomicNumber, elementDescription) {
 return `<!DOCTYPE html>
 <html lang="en">
@@ -22,6 +31,14 @@ return `<!DOCTYPE html>
 let createPost = postRespond()
 
 const server = http.createServer( (req, res) => {
+
+  //
+
+
+
+
+
+    // Begining of POST Requests
     if (req.method == 'POST') {
         req.on('data', (data) => {
             let content = data.toString()
@@ -35,7 +52,55 @@ const server = http.createServer( (req, res) => {
         });
         res.writeHead(200, {'Content-Type': 'application/json', 'success' : 'true', 'Date' : `${new Date().toUTCString()}`});
     };
-    res.end();
+    //res.end();
+
+    // Beginning of GET method
+    if(req.method === 'GET') {
+      let pathName = req.url;
+      let contentType;
+
+    if(req.url.indexOf('.html') > -1) {
+      pathName ='./public' + req.url;
+      contentType = 'text/html';
+    }
+    if(pathName === '/') {
+      pathName = './public/index.html';
+      contentType = 'text/html';
+    } else {
+      if(req.url.indexOf('.css') > -1) {
+        pathName = './public' + req.url;
+        contentType = 'text/css';
+      }
+    }
+
+    // Read contents of file
+    fs.readFile(pathName, (err, data)=> {
+      if(err) {
+        contentType = 'text/html';
+        fs.readFile('./public/404.html', (err, data) => {
+          res.writeHead(404, {
+            'Content-Type': contentType,
+          });
+          res.write(data.toString());
+          res.end();
+
+        });
+      } else {
+        res.writeHead(200, {
+          'Content-Type': contentType,
+        });
+        res.write(data.toString());
+        res.end();
+      }
+    });
+  }
+
 });
-server.listen(8080, '0.0.0.0')
+
+
+
+
+
+const port = 8080;
+server.listen(port)
 
